c9a90b8087014c198dc7e56f6cd98536473faa5d
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2024-07-31 18:55:27 +0300

    Complete task

--- src/main/java/assignments/LocalProcessor.java
+++ src/main/java/assignments/LocalProcessor.java
@@ -1,61 +1,69 @@
 package assignments;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.Scanner;
-
 import assignments.annotations.FullNameProcessorGeneratorAnnotation;
 import assignments.annotations.ListIteratorAnnotation;
 import assignments.annotations.ReadFullProcessorNameAnnotation;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 @Getter
 @Setter
 public class LocalProcessor {
+    private String processorVersion;
     private String processorName;
-    private Long period = 10000000000000L;
-    protected String ProcessorVersion;
-    private Integer valueofCheap;
-    Scanner informationscanner;
-    static LinkedList<String> stringArrayList = new LinkedList<>();
-
-    public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
-                          Scanner informationscanner, LinkedList<String> stringArrayList) {
-        this.processorName = processorName;
-        this.period = period;
-        ProcessorVersion = processorVersion;
-        this.valueofCheap = valueOfCheap;
-        this.informationscanner = informationscanner;
-        this.stringArrayList = stringArrayList;
-    }
+    private long period = 10_000_000_000_000L;
+    private Integer valueOfCheap;
+    private Scanner informationScanner;
+    private List<String> stringArrayList = new LinkedList<>();
 
     public LocalProcessor() {
     }
 
+    public LocalProcessor(String processorName, Long period, String processorVersion, Integer valueOfCheap,
+                          Scanner informationScanner, List<String> stringArrayList) {
+        this.processorName = processorName;
+        this.period = period;
+        this.processorVersion = processorVersion;
+        this.valueOfCheap = valueOfCheap;
+        this.informationScanner = informationScanner;
+        this.stringArrayList = stringArrayList;
+    }
+
     @ListIteratorAnnotation
-    public void listiterator(LinkedList<String> stringList) {
+    public void iterate(List<String> stringList) {
         stringArrayList = new LinkedList<>(stringList);
-        for (int i = 0; i < period; i++) {
-            System.out.println(stringArrayList.get(i).hashCode());
+        for (String element : stringArrayList) {
+            System.out.println(element.hashCode());
         }
     }
 
     @FullNameProcessorGeneratorAnnotation
-    public String fullnameProcessorgenerator(LinkedList<String> stringList) {
-        for (int i = 0; i < stringArrayList.size(); i++) {
-            processorName+=stringList.get(i)+' ';
+    public String fullNameProcessorGenerator(List<String> stringList) {
+        StringBuilder processorSB = new StringBuilder(processorName);
+        for (String element : stringList) {
+            processorSB.append(element);
         }
+        processorName = processorSB.toString();
         return processorName;
     }
 
     @ReadFullProcessorNameAnnotation
-    public void readfullprocessorname(File file) throws FileNotFoundException {
-            informationscanner = new Scanner(file);
-            while (informationscanner.hasNext()) {
-                ProcessorVersion+= informationscanner.nextLine();
+    public void readFullProcessorName(File file) throws FileNotFoundException {
+        StringBuilder processorVersionSB = new StringBuilder(processorVersion);
+        try (Scanner scanner = new Scanner(file)) {
+            informationScanner = scanner;
+            while (informationScanner.hasNext()) {
+                processorVersionSB.append(informationScanner.nextLine());
             }
-
+        } finally {
+            informationScanner = null;
+        }
+        processorVersion = processorVersionSB.toString();
     }
 }
